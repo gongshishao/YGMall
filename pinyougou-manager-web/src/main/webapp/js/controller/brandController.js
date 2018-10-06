e41b20cec4791ecba654c9b8c8e8b673dea1e9fe
gongshishao <gongshishao@126.com>
2018-10-07 00:02:11 +0800

    angularJS前端分层,控制层和业务层

--- pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -11,7 +11,7 @@ app.controller('brandController' ,function($scope,brandService){
 
     //分页
     $scope.findByPage = function (pageNum, pageSize) {
-        $http.get("../brand/findByPage.do?pageNum=" + pageNum + "&pageSize=" + pageSize).success(
+        brandService.findByPage(pageNum, pageSize).success(
             function (response) {
                 $scope.list = response.rows;//获得当前页数据,对应PageResult中的rows
                 $scope.paginationConf.totalItems = response.total;//更新总记录数,对应PageResult中的total
