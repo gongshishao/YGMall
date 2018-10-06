2ced2009815d8a93dbf9a72da17b3c87a632bc24
gongshishao <gongshishao@126.com>
2018-10-07 00:03:32 +0800

    angularJS前端分层,控制层和业务层

--- pinyougou-manager-web/src/main/webapp/js/service/brandService.js
+++ pinyougou-manager-web/src/main/webapp/js/service/brandService.js
@@ -6,11 +6,8 @@ app.service('brandService',function($http){
     }
 
     this.findByPage = function (pageNum, pageSize) {
-        $http.get("../brand/findByPage.do?pageNum=" + pageNum + "&pageSize=" + pageSize).success(
-            function (response) {
-                $scope.list = response.rows;//获得当前页数据,对应PageResult中的rows
-                $scope.paginationConf.totalItems = response.total;//更新总记录数,对应PageResult中的total
-            }
+       return $http.get("../brand/findByPage.do?pageNum=" + pageNum + "&pageSize=" + pageSize).success(
+
         );
     }
 });
