fc10c40c33ac509d38b3c44421d4d0f3e6e2b171
gongshishao <gongshishao@126.com>
2018-10-09 17:47:29 +0800

    实现springsercurity安全框架校验运营商登录

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.pinyougou.manager.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author admin13
+ * @version 1.0
+ * @description com.pinyougou.manager.controller
+ * @date 2018/10/9
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping("/name")
+    public Map<String, Object> name() {
+        //从springSecurity中获取登录名
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Map<String, Object> map = new HashMap<>();
+        map.put("loginName", name);
+        return map;
+    }
+}
