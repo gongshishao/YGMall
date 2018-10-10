bdc88c75de939f87c71837053e563c2811dadd90
gongshishao <gongshishao@126.com>
2018-10-10 23:01:58 +0800

    实现商家登录首页显示及注销

---
+++ pinyougou-shop-web/src/main/webapp/js/controller/indexController.js
@@ -0,0 +1,14 @@
+//控制层
+app.controller('indexController', function ($scope,loginService) {
+    $scope.showLoginName = function () {
+        loginService.loginName().success(
+            function (response) {
+                $scope.loginName = response.loginName;
+            }
+        );
+    }
+
+
+});
+
+
